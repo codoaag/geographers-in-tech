0825174394edbd1f27a73b76c606b1b04960e46a
Coline Dony <cdony@aag.org>
2017-09-21 11:00:08 -0400

    create a python template for API requests
    Create a python script that is a template to send API requests and read the response of the request.

---
+++ template_to_read_data_from_any_API.py
@@ -0,0 +1,22 @@
+# Purpose:  Read data from an API
+# Author:   Coline Dony
+# Date:     Sept 21, 2017
+
+import urllib2
+import json
+
+# For API's that require an API key
+# Assign your key as a string to this variable
+api_key = '<your api key>'
+
+# The URL with your API request
+# Request the response to be in the JSON format
+url_api_request = 'https://...'
+
+# The API request is sent using the internet
+# The JSON response is then read as a string
+api_response_string = urllib2.urlopen(url_api_request).read()
+
+# The JSON response string is now converted
+# The response is now in a dictionary format
+api_response = json.loads(api_response_string)
