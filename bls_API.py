2dbbd85798aec98c165ae651cc8aab817e96e77e
Coline Dony <cdony@aag.org>
2017-09-21 11:38:03 -0400

    Edit template to fit BLS API request format
    These first changes were made to the original template to fit the request format for the BLS API.
    
    The BLS API uses get and post commands, so we are using the requests library, rather than the urllib2 library.
    
    The content type needs to be specified as a header of the get command (we want the JSON fromat)
    
    the data request needs to be send it the form of a dictionary
    
    the request is then posted using the requests library
    
    The response does not need to be read, it is automatically assigned to the request variable
    
    we convert the text value of the response to a dictionary

--- bls_API.py
+++ bls_API.py
@@ -11,12 +11,14 @@ api_key = '<your api key>'
 
 # The URL with your API request
 # Request the response to be in the JSON format
-url_api_request = 'https://...'
+headers = {'Content-type': 'application/json'}
+data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
+url_api_request = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
 
 # The API request is sent using the internet
 # The JSON response is then read as a string
-api_response_string = urllib2.urlopen(url_api_request).read()
+# api_response_string = urllib2.urlopen(url_api_request).read()
 
 # The JSON response string is now converted
 # The response is now in a dictionary format
-api_response = json.loads(api_response_string)
+api_response = json.loads(url_api_request.text)
