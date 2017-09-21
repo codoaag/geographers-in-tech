087566b9f73a9377c2e35ca813be493690b33346
Coline Dony <cdony@aag.org>
2017-09-21 12:05:17 -0400

    Create script to request data from the O*Net API
    Create a Python script from which requests to the O*NET online API will be sent

--- Onet_API.py
+++ Onet_API.py
@@ -2,7 +2,7 @@
 # Author:   Coline Dony
 # Date:     Sept 21, 2017
 
-import requests
+import urllib2
 import json
 
 # For API's that require an API key
@@ -11,14 +11,12 @@ api_key = '<your api key>'
 
 # The URL with your API request
 # Request the response to be in the JSON format
-headers = {'Content-type': 'application/json'}
-data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
-url_api_request = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+url_api_request = 'https://...'
 
 # The API request is sent using the internet
 # The JSON response is then read as a string
-# api_response_string = urllib2.urlopen(url_api_request).read()
+api_response_string = urllib2.urlopen(url_api_request).read()
 
 # The JSON response string is now converted
 # The response is now in a dictionary format
-api_response = json.loads(url_api_request.text)
+api_response = json.loads(api_response_string)
